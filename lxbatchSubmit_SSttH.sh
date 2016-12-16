625cf5673f495e7abcf1e9f8779c841971812f12
Joshuha Thomas-Wilsker <joshuha.thomas-wilsker@cern.ch>
2016-12-16 12:37:56 +0100

    new repo

---
+++ lxbatchSubmit_SSttH.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Lxplus Batch Job Script
+# > Don't forget . . . whitespace matters ;)
+
+CMSSW_SRC=$(echo ${CMSSW_BASE})/src
+WORKING_DIR=$(echo ${CMSSW_BASE})/src/TTH/ttHACIHEP
+
+export CMSSW_BASE=$CMSSW_SRC
+cd $CMSSW_SRC
+eval `scramv1 runtime -sh`
+
+cd $WORKING_DIR
+echo $WORKING_DIR
+
+#====== cmsRun Config File =======
+#sleep 10
+
+eval ./SecondStep ttHbb_MC
