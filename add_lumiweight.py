dc6feafe648fda2ab9c7704e8c63c5ecf6b03da5
Joshuha Thomas-Wilsker <joshuha.thomas-wilsker@cern.ch>
2017-03-21 17:12:36 +0100

    adding add_lumiweight.py

--- add_lumiweight.py
+++ add_lumiweight.py
@@ -13,7 +13,8 @@ W = (XS * BR) / N0
 Would then multiply by L(data) to rescale sample.
 '''
 
-import ROOT, sys, math, os, array
+import ROOT, sys, math, os,
+from array import array
 workingDir = os.getcwd()
 inputDir = '/publicfs/cms/data/TopQuark/ttHbb/JTW/2017_03/ttHACIHEP/output/'
 inputFiles = ["MC/ttjets_altTUNE/ttjets_altTUNE_Merged_rootplas.root", "MC/ttHbb/ttHbb_Merged_rootplas.root"]
