d16fd3ef9fcdaaede78131d9246b8ded652c8438
Joshuha Thomas-Wilsker <joshuha.thomas-wilsker@cern.ch>
2017-03-21 17:17:16 +0100

    adding add_lumiweight.py

--- add_lumiweight.py
+++ add_lumiweight.py
@@ -19,12 +19,12 @@ workingDir = os.getcwd()
 inputDir = '/publicfs/cms/data/TopQuark/ttHbb/JTW/2017_03/ttHACIHEP/output/'
 inputFiles = ["MC/ttjets_altTUNE/ttjets_altTUNE_Merged_rootplas.root", "MC/ttHbb/ttHbb_Merged_rootplas.root"]
 
-XS = {
+XS_dict = {
     "ttjets_SL_Merged_rootplas":831.76,
     "ttHbb_Merged_rootplas":0.5071
 }
 
-BR = {
+BR_dict = {
     "ttjets_SL_Merged_rootplas":1,
     "ttHbb_Merged_rootplas":0.5824
 }
@@ -42,15 +42,20 @@ for file0 in inputFiles:
     lumiweight_branch = ttree.Branch("lumiweight",lumiweight_, "lumiweight/F")
 
     N0 = 0
+    XS = 0
+    BR = 0
     N0 = ttree.GetEntries()
     print '# Entries = ' , N0
 
-    for key,value in XS.iteritems():
+    for key,value in XS_dict.iteritems():
         if key in file0:
             print 'Got XS = ', value
-    for key,value in BR.iteritems():
+            XS = value
+
+    for key,value in BR_dict.iteritems():
         if key in file0:
             print 'Got BR = ', value
+            BR = value
 
     W = (XS*BR)/N0
     print 'Weight for this sample = ', W
