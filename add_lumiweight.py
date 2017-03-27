6f691f1e4a4bf54b155163be92a6c6603141c9e1
Joshuha Thomas-Wilsker <joshuha.thomas-wilsker@cern.ch>
2017-03-27 13:03:29 +0200

    add_lumiweight minor file list update

--- add_lumiweight.py
+++ add_lumiweight.py
@@ -29,20 +29,19 @@ import ROOT, sys, math, os
 from array import array
 workingDir = os.getcwd()
 
-ttjets_HF_filter = '/publicfs/cms/data/TopQuark/cms13TeV/FullMoriond2017/mc/TTToSemilepton_ttbbFilter_TuneCUETP8M2_ttHtranche3_13TeV-powheg-pythia8/crab_FullMorV1_TTToSemilepton_ttbbFilter/170217_064724/0000/'
+#ttjets_HF_filter = '/publicfs/cms/data/TopQuark/cms13TeV/FullMoriond2017/mc/TTToSemilepton_ttbbFilter_TuneCUETP8M2_ttHtranche3_13TeV-powheg-pythia8/crab_FullMorV1_TTToSemilepton_ttbbFilter/170217_064724/0000/'
 
 #====== Uncomment before committing to git -> For running on IHEP farm.
-inputDir = '/publicfs/cms/data/TopQuark/ttHbb/JTW/2017_03/ttHACIHEP/output/MC/'
-#inputDir = '/publicfs/cms/data/TopQuark/cms13TeV/FullMoriond2017/mc/'
+#inputDir = '/publicfs/cms/data/TopQuark/ttHbb/JTW/2017_03/ttHACIHEP/output/MC/'
 
-inputFiles = ["ttHbb/ttHbb_Merged_rootplas.root"
+inputFiles = [#"ttHbb/ttHbb_Merged_rootplas.root"
               #"ttH_nonbb/ttH_nonbb_Merged_rootplas.root",
               #"ttjets_incl/ttjets_incl_Merged_rootplas.root",
               #"ttjets_SL/ttjets_SL_Merged_rootplas.root",
               #"ttjets_DL/ttjets_DL_Merged_rootplas.root",
               ##"ttjets_bfilter/ttjets_bfilter_Merged_rootplas.root",
               #"stop_schan/stop_schan_Merged_rootplas.root",
-              #"stop_tchan_top/stop_tchan_top_Merged_rootplas.root",
+              "stop_tchan_top/stop_tchan_top_Merged_rootplas.root"
               #"stop_tchan_antitop/stop_tchan_antitop_Merged_rootplas.root",
               #"stop_tw_top/stop_tw_top_Merged_rootplas.root",
               #"stop_tW_antitop/stop_tW_antitop_Merged_rootplas.root",
@@ -60,8 +59,8 @@ inputFiles = ["ttHbb/ttHbb_Merged_rootplas.root"
               ]
 
 #========= Ucomment here for testing on lxplus
-#inputDir = '/afs/cern.ch/work/j/jthomasw/private/IHEP/CMSSW/CMSSW_8_0_26_patch2/src/TTH/ttHACIHEP/output/'
-#inputFiles = ["ttHbb_Merged_rootplas.root","ttjets_altTUNE_Merged_rootplas.root"]
+inputDir = '/afs/cern.ch/work/j/jthomasw/private/IHEP/CMSSW/CMSSW_8_0_26_patch2/src/TTH/ttHACIHEP/output/stop_tchan_top/'
+inputFiles = ["stop_tchan_top_Merged_rootplas.root"]
 
 XS_dict = {
     "ttHbb_Merged_rootplas":0.5071,
@@ -124,9 +123,6 @@ for file0 in inputFiles:
         if key in file0:
             output_tfile_name = key
 
-    # Construct new variable.
-    lumiweight_ = array('f',[0.])
-
     tfile = ROOT.TFile(inputFullPath,'Update')
     ttree = tfile.Get("BOOM")
 
@@ -136,14 +132,16 @@ for file0 in inputFiles:
     for quick_ev in range(ttree.GetEntries()):
 
         # get the next tree in the chain and verify
-        ientry = ttree.LoadTree( quick_ev )
-        if ientry < 0:
-            break
+        #ientry = ttree.LoadTree( quick_ev )
+        #if ientry < 0:
+        #    break
         # copy next entry into memory and verify
         nb = ttree.GetEntry( quick_ev )
         #print 'EVENT_genWeight = ', ttree.EVENT_genWeight
         swg += ttree.EVENT_genWeight
 
+    # Construct array for new variable.
+    lumiweight_ = array('f',[0])
     b_lumiweight = ttree.Branch("lumiweight",lumiweight_, "lumiweight/F")
     ttree.SetBranchStatus("lumiweight",1)
 
