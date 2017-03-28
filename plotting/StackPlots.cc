6817b068be28446bc89c3c824b84a471466e4d34
Joshuha Thomas-Wilsker <joshuha.thomas-wilsker@cern.ch>
2017-03-28 17:05:05 +0200

    StackPlots.cc

--- plotting/StackPlots.cc
+++ plotting/StackPlots.cc
@@ -446,7 +446,7 @@ void draw_plots(TCanvas* c1, TH1F* h_sum_var, THStack* hstack, TH1F* h_data_var,
  stringstream Title_ss;
  Title_ss << "#scale[0.90]{CMS preliminary,   #sqrt{s} = 13 TeV, L = " << Luminosity <<" fb^{-1}}";
  //string Title_s = Title_ss.string();
- const char* Plot_Title = Title_ss.string().c_str();
+ const char* Plot_Title = Title_ss.str().c_str();
  if(!show_ratio) h_data_var->GetXaxis()->SetTitle(vartitle.c_str());
  if(show_title)  h_data_var->SetTitle(Plot_Title);
  if(h_data_var->GetEntries()==0) gStyle->SetOptStat(0);
