a508156de4810e4d04acd1ef02084dc88cae13a6
Joshuha Thomas-Wilsker <joshuha.thomas-wilsker@cern.ch>
2017-04-05 13:41:12 +0200

    plotting StackPlots.cc

--- plotting/StackPlots.cc
+++ plotting/StackPlots.cc
@@ -69,10 +69,10 @@ const double scale      = 0;    //0 means no scaling; any other values means sca
 
 // ===== Normalisation of plots =====
 // One must run the script once with "normalised = false" to get the value for the background normalisation.
-const bool normalised   = false;
-const double normbkg    = 7.38942e+06; //normbkg and normdata values have to be taken after 1 iteration of the macro with normalised = false
+const bool normalised   = true;
+const double normbkg    = 1.18903e+08; //normbkg and normdata values have to be taken after 1 iteration of the macro with normalised = false
 const double normdata   = 516742;
-const double normsig    = 2.84875e+06;
+const double normsig    = 138624;
 
 // ===== Plots =====
 const bool save_plots   = true;
