a6ddacaf3d09d152e4371d5f1668c1f0d3c7b10d
Joshuha Thomas-Wilsker <joshuha.thomas-wilsker@cern.ch>
2017-04-05 13:25:50 +0200

    plotting StackPlots.cc

--- plotting/StackPlots.cc
+++ plotting/StackPlots.cc
@@ -69,7 +69,7 @@ const double scale      = 0;    //0 means no scaling; any other values means sca
 
 // ===== Normalisation of plots =====
 // One must run the script once with "normalised = false" to get the value for the background normalisation.
-const bool normalised   = false;
+const bool normalised   = true;
 const double normbkg    = 7.38942e+06; //normbkg and normdata values have to be taken after 1 iteration of the macro with normalised = false
 const double normdata   = 516742;
 const double normsig    = 2.84875e+06;
@@ -426,8 +426,8 @@ TH1F* double_h_var(unsigned int v, string var, string varT, uint i, string rootp
   else                         hist_err = new TH1F("hist_err","hist_err",bin[v],inRange[v],endRange[v]);
   hist_err->Sumw2();
 
-  //for(int j=0; j<tree->GetEntries(); j++)
-  for(int j=0; j<5; j++)
+  for(int j=0; j<tree->GetEntries(); j++)
+  //for(int j=0; j<5; j++)
   {
     double w = 1.;
     Long64_t tentry = tree->LoadTree(j);
