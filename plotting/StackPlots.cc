ae4b83659241ed7e3e61212840b6074d92c9fccd
Joshuha Thomas-Wilsker <joshuha.thomas-wilsker@cern.ch>
2017-04-10 15:15:14 +0200

    Liberal ranges on for variables in StackPlots.cc to get scaling values

--- plotting/StackPlots.cc
+++ plotting/StackPlots.cc
@@ -71,7 +71,7 @@ const double scale      = 0;    //0 means no scaling; any other values means sca
 
 // ===== Normalisation of plots =====
 // One must run the script once with "normalised = false" to get the value for the background normalisation.
-const bool normalised   = true;
+const bool normalised   = false;
 const double normbkg    = /*1.11627e+08;*/1.86236e+06; //normbkg and normdata values have to be taken after 1 iteration of the macro with normalised = false
 const double normdata   = 516742;
 const double normsig    = /*130403;*/388904;
@@ -162,7 +162,7 @@ const int    bin[numVar]        = {
   40,
   20,
   20
-};
+};/*
 const double inRange[numVar]    = {
   -5,
   0,
@@ -208,30 +208,30 @@ const double endRange[numVar]   = {
   200,
   3,
   4
-};
-/*
+};*/
+
 const double inRange[numVar]    = {
-  -10,
-  0,
-  0,
-  0,
-  0,
-  0,
-  4,
-  2,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  0,
-  -10,
-  -10,
-  0,
-  -10,
-  -10
+  -100,
+  -100,
+  -100,
+  -100,
+  -100,
+  -100,
+  -100,
+  -100,
+  -100,
+  -100,
+  -100,
+  -100,
+  -100,
+  -100,
+  -100,
+  -100,
+  -100,
+  -100,
+  -100,
+  -100,
+  -100
 };
 const double endRange[numVar]   = {
   20,
@@ -255,7 +255,7 @@ const double endRange[numVar]   = {
   500,
   10,
   10
-};*/
+};
 
 /////
 //   Declare functions
