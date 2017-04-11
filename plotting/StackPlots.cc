283cb813d4f426964b953e480d2f444fe96e6655
Joshuha Thomas-Wilsker <joshuha.thomas-wilsker@cern.ch>
2017-04-11 10:57:57 +0200

    add debug lines StackPlots.cc

--- plotting/StackPlots.cc
+++ plotting/StackPlots.cc
@@ -87,7 +87,7 @@ const int logYscale[numVar] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 const int    col_size   = 500; //>= highest bin
 
 // Number of variables you want to loop over:
-const unsigned int ini_var = 20;
+const unsigned int ini_var = 0;
 const unsigned int fin_var = 21;
 const int posvtcr          = 0;
 
