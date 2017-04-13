57d039354b240152f430a473f517a4b3c3a284ac
Joshuha Thomas-Wilsker <joshuha.thomas-wilsker@cern.ch>
2017-04-13 17:06:32 +0200

    removed old bJetness vars from StackPlots.cc

--- plotting/StackPlots.cc
+++ plotting/StackPlots.cc
@@ -71,7 +71,7 @@ const double scale      = 0;    //0 means no scaling; any other values means sca
 
 // ===== Normalisation of plots =====
 // One must run the script once with "normalised = false" to get the value for the background normalisation.
-const bool normalised   = false;
+const bool normalised   = true;
 //const double normbkg    = 2.81681e+07; //normbkg and normdata values have to be taken after 1 iteration of the macro with normalised = false
 //const double normdata   = 516742;
 //const double normsig    = 17824.5;
@@ -90,7 +90,7 @@ const int    col_size   = 500; //>= highest bin
 
 // Number of variables you want to loop over:
 const unsigned int ini_var = 0;
-const unsigned int fin_var = 19;
+const unsigned int fin_var = 38;
 const int posvtcr          = 0;
 
 
