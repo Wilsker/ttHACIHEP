a549b1c7256b7c9090ff7d6db9941751f8ff5869
Wilsker <joshuha.thomas-wilsker@cern.ch>
2017-05-04 16:43:49 +0200

    Stackplots.cc

--- plotting/StackPlots.cc
+++ plotting/StackPlots.cc
@@ -90,7 +90,7 @@ const int    col_size   = 500; //>= highest bin
 
 // Number of variables you want to loop over:
 const unsigned int ini_var = 62;
-const unsigned int fin_var = 73;
+const unsigned int fin_var = 63;
 const int posvtcr          = 0;
 
 // ======= Variables map ======= ???
