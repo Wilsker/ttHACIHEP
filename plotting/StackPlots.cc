8041c3d7df134f39f3d768e45753e709eb9df587
Wilsker <joshuha.thomas-wilsker@cern.ch>
2017-05-16 10:02:46 +0200

    StackPlots.cc add bWeights

--- plotting/StackPlots.cc
+++ plotting/StackPlots.cc
@@ -71,7 +71,7 @@ const double scale      = 0;    //0 means no scaling; any other values means sca
 
 // ===== Normalisation of plots =====
 // One must run the script once with "normalised = false" to get the value for the background normalisation.
-const bool normalised   = true;
+const bool normalised   = false;
 //const double normbkg    = 2.81681e+07; //normbkg and normdata values have to be taken after 1 iteration of the macro with normalised = false
 //const double normdata   = 516742;
 //const double normsig    = 17824.5;
@@ -89,8 +89,8 @@ const int logYscale[numVar] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 const int    col_size   = 500; //>= highest bin
 
 // Number of variables you want to loop over:
-const unsigned int ini_var = 56;
-const unsigned int fin_var = 57;
+const unsigned int ini_var = 36;
+const unsigned int fin_var = 37;
 const int posvtcr          = 0;
 
 // ======= Variables map ======= ???
