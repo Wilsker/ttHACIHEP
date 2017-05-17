a6662b1c72b86aaa178dcddb4f91228ffcd666e1
Wilsker <joshuha.thomas-wilsker@cern.ch>
2017-05-17 12:23:35 +0200

    Stackplots.cc adding errors to ratio plot

--- plotting/StackPlots.cc
+++ plotting/StackPlots.cc
@@ -1348,7 +1348,7 @@ void draw_plots(TCanvas* c1, TH1F* h_sum_var, THStack* hstack, TH1F* h_data_var,
     ratio_plot->GetYaxis()->SetTitleSize(20);
     ratio_plot->GetYaxis()->SetTitleFont(43);
     ratio_plot->GetYaxis()->SetTitleOffset(1.55);
-    ratio_plot->GetYaxis()->SetLabelSize(20);
+    ratio_plot->GetYaxis()->SetLabelSize(10);
     ratio_plot->GetYaxis()->SetLabelFont(43);
 
     TLine* line = new TLine(inRange[v],1,endRange[v],1);
