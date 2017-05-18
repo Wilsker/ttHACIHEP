51dd6b92747b79a0940e027db6befccc4f3112f6
Wilsker <joshuha.thomas-wilsker@cern.ch>
2017-05-18 16:46:25 +0200

    added stop_schan to add_lumiweight

--- add_lumiweight.py
+++ add_lumiweight.py
@@ -34,13 +34,13 @@ workingDir = os.getcwd()
 #====== Uncomment before committing to git -> For running on IHEP farm.
 inputDir = '/publicfs/cms/data/TopQuark/ttHbb/JTW/2017_03/ttHACIHEP/output/MC/'
 
-inputFiles = ["ttHbb/ttHbb_Merged_rootplas.root",
+inputFiles = [#"ttHbb/ttHbb_Merged_rootplas.root",
               #"ttH_nonbb/ttH_nonbb_Merged_rootplas.root",
-              "ttjets_incl/ttjets_incl_Merged_rootplas.root"
+              #"ttjets_incl/ttjets_incl_Merged_rootplas.root"
               #"ttjets_SL/ttjets_SL_Merged_rootplas.root",
               #"ttjets_DL/ttjets_DL_Merged_rootplas.root",
               #"ttjets_bfilter/ttjets_bfilter_Merged_rootplas.root",
-              #"stop_schan/stop_schan_Merged_rootplas.root",
+              "stop_schan/stop_schan_Merged_rootplas.root",
               #"stop_tchan_top/stop_tchan_top_Merged_rootplas.root",
               #"stop_tchan_antitop/stop_tchan_antitop_Merged_rootplas.root",
               #"stop_tw_top/stop_tw_top_Merged_rootplas.root",
