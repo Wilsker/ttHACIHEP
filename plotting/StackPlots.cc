49f9f2c75618922aaeaeec5d5568ca045df56306
Wilsker <joshuha.thomas-wilsker@cern.ch>
2017-06-15 15:26:17 +0200

    StackPlots.cc

--- plotting/StackPlots.cc
+++ plotting/StackPlots.cc
@@ -72,7 +72,7 @@ const double scale      = 0;    //0 means no scaling; any other values means sca
 
 // ===== Normalisation of plots =====
 // One must run the script once with "normalised = false" to get the value for the background normalisation.
-const bool normalised   = false;
+const bool normalised   = true;
 //normbkg and normdata values have to be taken after 1 iteration of the macro with normalised = false
 double normbkg;
 double normdata;
@@ -934,9 +934,9 @@ TH1F* double_h_var(unsigned int v, string var, string varT, uint i, string rootp
       normsig = 17017.5;
     }
     else if(var.find("BJetness")!=std::string::npos){
-      normbkg = 2.76786e+07;
-      normdata= 660109;
-      normsig = 17518.3;
+      normbkg = 2.62943e+07;
+      normdata= 528946;
+      normsig = 17017.8;
     }
     else if(var.find("lead_el")!=std::string::npos){
       normbkg = 1.0663e+07;
@@ -1087,9 +1087,9 @@ TH1F* int_h_var(unsigned int v, string var, string varT, uint i, string rootplas
       normsig = 632877;
     }
     else if(var.find("BJetness")!=std::string::npos){
-      normbkg = 2.76786e+07;
-      normdata= 660109;
-      normsig = 17518.3;
+      normbkg = 2.62943e+07;
+      normdata= 528946;
+      normsig = 17017.8;
     }
     else if(var.find("lead_el")!=std::string::npos){
       normbkg = 1.0663e+07;
@@ -1268,9 +1268,9 @@ TH1F* vector_double_h_var(unsigned int v, string var, string varT, uint i, strin
       normsig = 597462;
     }
     else if(var.find("BJetness")!=std::string::npos){
-      normbkg = 2.76786e+07;
-      normdata= 660109;
-      normsig = 17518.3;
+      normbkg = 2.62943e+07;
+      normdata= 528946;
+      normsig = 17017.8;
     }
     else if(var.find("lead_el")!=std::string::npos){
       normbkg = 1.0663e+07;
