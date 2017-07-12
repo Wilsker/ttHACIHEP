309c5940abae9366c82e1c143bb5d109eb5b763d
Wilsker <joshuha.thomas-wilsker@cern.ch>
2017-07-12 17:55:33 +0200

    Stackplots.cc edit for norm calc.

--- plotting/StackPlots.cc
+++ plotting/StackPlots.cc
@@ -84,8 +84,8 @@ const int logYscale[numVar] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
 const int    col_size   = 500; //>= highest bin
 
 // Number of variables you want to loop over:
-const unsigned int ini_var = 21;
-const unsigned int fin_var = 22;
+const unsigned int ini_var = 41;
+const unsigned int fin_var = 42;
 const int posvtcr          = 0;
 
 // ======= Variables map ======= ???
